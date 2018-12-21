d7cf9a715b094b81e1c76b5c44d93e25c8426ba8
Hsin-Li Chu <jameschu@cml16.csie.ntu.edu.tw>
2018-12-21 13:01:02 +0800

    fix bug

--- hw4/hw4_train.sh
+++ hw4/hw4_train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #bash hw4_train.sh <train_x file> <train_y file> <test_x.csv file> <dict.txt.big file>
-python3 preprocess.py $4 $1 train_x_processed.plk
+python3 w2v_preprocess.py $4 $1 $3 
 python3 model.py 0 $2
